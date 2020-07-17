9b93da5dece7a4f1509698c572473d8bf46138c4
ViRuS_X_Z <53702257+virus-x-z@users.noreply.github.com>
2020-07-17 01:02:48 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+### VXZ-ST-Tool
+---
+#### Install
+* Linux & Termux
+```bash
+sudo apt update
+sudo apt upgrade
+sudo apt install python3
+```
+#### Put the Python files in one folder named (tools)
+
+To start VXZ-ST-Tool ===>
+`python3 VXZ-ST-Tool.py`
---
+++ VXZ-ST-Tool.py
@@ -0,0 +1,24 @@
+# ViRuS-X-Z Team
+# VXZ-Tools
+# VXZ-Safe-Tools
+
+# imports
+import os
+from Functions import *
+import compileall
+
+# Start-script-programming
+loading0 ()
+banner3 ()
+login ()
+print ("Welcome in VXZ-Safe-Tools")
+print ("    ")
+
+# V ========== X ========== Z
+
+# Tools' Folders To Safe ^_^
+compileall.compile_dir("tools")
+
+# V ========== X ========== Z
+# V ========== X ========== Z
+# E ========== N ========== D
